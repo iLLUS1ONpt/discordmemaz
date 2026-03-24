40925edc2e81cf483a0c84e204feb5fd0f6bb972
iLLUS1ONpt <34459615+iLLUS1ONpt@users.noreply.github.com>
2026-03-24 00:53:11 -0100

    Enhance teleportation admin tool with UI updates
    Refactor teleportation script to improve UI and event handling.

--- admin_with_teleporter.lua
+++ admin_with_teleporter.lua
@@ -1,192 +1,267 @@
--- A simple script allowing admin to teleport people around with a bunch of comments.
--- Please feel free to use it as an example for something more complex.
-
--- It might be a good idea generally to keep reference to sim state nearby. Not that it’s that
--- important for opmitization, but it helps a tiny bit and makes code nicer:
-local sim = ac.getSim()
-
--- The way it would work is by using `ac.OnlineEvent`. Remember: online scripts ran on clients,
--- not knowing what other scripts are up to. This is a simple way to allow them to exchange
--- messages.
--- • At “backend” it uses certain hidden chat messages to exchange data. Might look a bit messy
---   if somebody would to join without CSP, although even then messages would not be that visible.
---   And with custom AC server implementations a more direct and optimized way might be used. But
---   in any way, don’t spam those events too much.
--- • When defining online event like this, you need to specify its data layout (what fields it would
---   have and what types those fields are). Generally it’s a good idea to keep types smaller where
---   possible, but for smaller data packets it doesn’t really matter that much.
--- • Second parameter is a function which will be ran when this client receives an event. Also,
---   important thing to note: currently events work in broadcasting manner, if sent, everybody
---   receives it.
--- • Created `ac.OnlineEvent()` acts like a function. Simply call it and fill out the table with data
---   to send out the message.
--- • It’s important to keep data layout the same between all clients. This is how ID of an event
---   is generated.
-local jumpEvent = ac.OnlineEvent({
-  key = ac.StructItem.key('jumpSomewhere'),  -- to make sure there would be no collisions with other events, it’s a good idea to use a unique key
-  targetSessionID = ac.StructItem.int32(),   -- since messages are broadcasted to everybody, we need to specify who we mean
-  targetPosition = ac.StructItem.vec3(),     -- 3-dimensional vector for target position
-  targetDirection = ac.StructItem.vec3()     -- 3-dimensional vector for target orientation
-}, function (sender, message)
-  -- This is the function that will be called when this client receives an event message. First argument,
-  -- `sender`, points to `ac.StateCar` of the sender or set to `nil` if message has come from server. Second
-  -- argument is the message itself.
-  if message.targetSessionID == ac.getCar(0).sessionID then
-    -- Note: online, each car has two identifiers. One is its regular index, the one that’s always 0 for current player. First car
-    -- is always player’s car. Second is `sessionID`: that is the index in online entry list. So, while first-ID-car-index will
-    -- vary for the same cars on different clients, `sessionID` is always synced. That line above this comment simply makes sure
-    -- `targetSessionID` from the message matches with `sessionID` for player’s car.
-
-    -- And this is where movement occurs. Notice that we’re moving player’s car. Online, there is absolutely no point in moving
-    -- anything else: physics for remote cars runs on their corresponding PCs and there is no way we can affect it directly.
-    physics.setCarPosition(0, message.targetPosition, message.targetDirection)
-  end
-end)
-
--- This is all for “client” side of things, now let’s add some UI to be able to be able to emit that event message and
--- pick a car and a destination point. For that, we’ll use `ui.registerOnlineExtra()` function. Usually online scripts
--- are UI-less: you can create HUD, but without many interactive elements (unless you’d track mouse coordinates and state
--- manually). But with `ui.registerOnlineExtra()` you can create new items in lightbulb section or admin section of
--- the new chat app.
-
--- First, let’s define some variables related to HUD state. Here is selected car:
-local selectedCar = nil ---@type ac.StateCar
-
--- And selected destination, relative to map.png, 0…1 range
-local teleportPoint = vec2(0.5, 0.5)
-
--- And let’s load some info about track map. That’s where it’s image is:
-local mapFilename = ac.getFolder(ac.FolderID.ContentTracks)..'/'..ac.getTrackFullID('/')..'/map.png'
-
--- This is how its parameters can be read. Just load `ac.INIConfig` and map it into a simple and neat table.
--- It would even have full documentation support with that VSCode plugin:
-local mapParams = ac.INIConfig.load(ac.getFolder(ac.FolderID.ContentTracks)..'/'..ac.getTrackFullID('/')..'/data/map.ini'):mapSection('PARAMETERS', {
-  X_OFFSET = 0,  -- by providing default values script also specifies type, so that values can be parsed properly
-  Z_OFFSET = 0,
-  WIDTH = 600,
-  HEIGHT = 600
+local config = ac.configValues({
+    minimumSpeed = 50,
+    showUI = false,
+    collisionMessages = "",
+    overtakeMessages = "",
+    closeOvertakeMessages = ""
 })
 
--- And last, size of the map. We could calculate it each frame, but it’s nicer if done this way:
-local mapSize = vec2(mapParams.WIDTH / mapParams.HEIGHT * 200, 200)
+local collisionMessages = stringify.parse(config.collisionMessages)
+local overtakeMessages = stringify.parse(config.overtakeMessages)
+local closeOvertakeMessages = stringify.parse(config.closeOvertakeMessages)
 
--- A simple helper function which would take a 2-dimensional vector relative to map.png and turn it into world coordinates:
-local function getWorldPosFromRelativePos(relativePos)
-  -- Doing X and Z is not a problem, but vertical Y axis is a bit trickier. Set it to 0 for now:
-  local ret = vec3(relativePos.x * mapParams.WIDTH - mapParams.X_OFFSET, 0, relativePos.y * mapParams.HEIGHT - mapParams.Z_OFFSET)
+ui.registerOnlineExtra(ui.Icons.CarFront, "Overtake Run", nil, nil, function (okClicked)
+  config.showUI = not config.showUI
+end)
 
-  -- Convert resulting coordinates to track spline progress from 0 to 1:
-  local trackProgress = ac.worldCoordinateToTrackProgress(ret)
+-- Event state:
+local timePassed = 0
+local totalScore = 0
+local comboMeter = 1
+local comboColor = 0
+local currentRank = 0
 
-  -- Convert track spline progress back to world coordinates:
-  local nearestOnTrack = ac.trackProgressToWorldCoordinate(trackProgress)
+local personalBest = 0
+local ownRank = 0
 
-  -- And let’s just grab Y value from there. Not the best approach, but should work for most cases:
-  ret.y = nearestOnTrack.y
-  return ret
+local messages = {}
+local glitter = {}
+local glitterCount = 0
+
+local overtakeEventType = {
+    NONE = 0,
+    OVERTAKE = 1,
+    COLLISION = 2,
+    FINISHED = 3,
+    TOO_SLOW = 4,
+    CLOSE_OVERTAKE = 5
+}
+
+local function addMessage(text, mood)
+  for i = math.min(#messages + 1, 4), 2, -1 do
+    messages[i] = messages[i - 1]
+    messages[i].targetPos = i
+  end
+  messages[1] = { text = text, age = 0, targetPos = 1, currentPos = 1, mood = mood }
+  if mood == 1 then
+    for i = 1, 60 do
+      local dir = vec2(math.random() - 0.5, math.random() - 0.5)
+      glitterCount = glitterCount + 1
+      glitter[glitterCount] = { 
+        color = rgbm.new(hsv(math.random() * 360, 1, 1):rgb(), 1), 
+        pos = vec2(80, 140) + dir * vec2(40, 20),
+        velocity = dir:normalize():scale(0.2 + math.random()),
+        life = 0.5 + 0.5 * math.random()
+      }
+    end
+  end
 end
 
--- This is the function that will create UI for the teleporting tool. Something very basic:
-local function teleportHUD()
-  -- Firstly, let’s show the list of players:
-  ui.text('Select somebody:')
+local numUpdates = 0
+local overtakeUpdateEvent = ac.OnlineEvent({
+    ac.StructItem.key("overtakeUpdate"),
+    score = ac.StructItem.int64(),
+    combo = ac.StructItem.float(),
+    events = ac.StructItem.array(ac.StructItem.byte(), 10),
+    rank = ac.StructItem.int32()
+}, function (sender, message)
+    if sender ~= nil then return end
 
-  -- A simple way to do a scrolling list, 120 pixels in height. Make sure ID (first argument) is unique
-  -- within your script:
-  ui.childWindow('##drivers', vec2(ui.availableSpaceX(), 120), function ()
-    -- Iterate through all cars:
-    for i = 1, sim.carsCount do
-      -- Get a car (`ac.getCar()` works with 0-based indices):
-      local car = ac.getCar(i - 1)
+    numUpdates = numUpdates + 1
+    ac.debug("No. of Updates", numUpdates)
 
-      -- Only offer to teleport connected cars:
-      if car.isConnected then
+    comboMeter = tonumber(message.combo)
+    totalScore = tonumber(message.score)
+    currentRank = tonumber(message.rank)
 
-        -- This function creates a selectable item. When clicked, it returns `true`, that’s where
-        -- we change selected car:
-        if ui.selectable(ac.getDriverName(i - 1), selectedCar == car) then
-          selectedCar = car
+    if totalScore > personalBest then
+        personalBest = totalScore
+    end
+
+    for i = 0, 9 do
+        if     message.events[i] == overtakeEventType.NONE           then break
+        elseif message.events[i] == overtakeEventType.OVERTAKE       then addMessage(overtakeMessages[math.random(#overtakeMessages)], 0)
+        elseif message.events[i] == overtakeEventType.CLOSE_OVERTAKE then addMessage(closeOvertakeMessages[math.random(#closeOvertakeMessages)], 1)
+        elseif message.events[i] == overtakeEventType.TOO_SLOW       then addMessage("Speed up!", -1)
+        elseif message.events[i] == overtakeEventType.COLLISION      then addMessage(collisionMessages[math.random(#collisionMessages)], -1)
         end
+    end
+end)
 
+local overtakePersonalBestEvent = ac.OnlineEvent({
+    ac.StructItem.key("overtakePersonalBest"),
+    score = ac.StructItem.int64(),
+    rank = ac.StructItem.int32()
+}, function (sender, message)
+    if sender ~= nil then return end
+
+    personalBest = tonumber(message.score)
+    ownRank = tonumber(message.rank)
+end)
+
+overtakePersonalBestEvent({})
+
+function script.update(dt)
+  if not config.showUI then return end
+
+  local comboFadingRate = 0.5 * math.lerp(1, 0.1, math.lerpInvSat(car.speedKmh, 80, 200))
+  comboMeter = math.max(1, comboMeter - dt * comboFadingRate)
+end
+
+local function updateMessages(dt)
+  comboColor = comboColor + math.min(25, dt * 3 * comboMeter)
+  if comboColor > 360 then comboColor = comboColor - 360 end
+  for i = 1, #messages do
+    local m = messages[i]
+    m.age = m.age + dt
+    m.currentPos = math.applyLag(m.currentPos, m.targetPos, 0.8, dt)
+  end
+  for i = glitterCount, 1, -1 do
+    local g = glitter[i]
+    g.pos:add(g.velocity)
+    g.velocity.y = g.velocity.y + 0.02
+    g.life = g.life - dt
+    g.color.mult = math.saturate(g.life * 4)
+    if g.life < 0 then
+      if i < glitterCount then
+        glitter[i] = glitter[glitterCount]
       end
+      glitterCount = glitterCount - 1
     end
-
-    -- Note: we could also use dropdown list with `ui.combo()`, but those extra online things can’t draw
-    -- them yet unless you’d use `ui.OnlineExtraFlags.Tool` flag. To combine both Tool and Admin flag, use
-    -- bit library: `bit.bor(ui.OnlineExtraFlags.Admin, ui.OnlineExtraFlags.Tool)` (or just sum them, but
-    -- in more complicated cases it might not work, so might be a good idea to get used to bit library).
-  end)
-
-  -- Next, let’s draw a nice interactive map. Something simple but informative:
-  ui.text('Select point on a map:')
-
-  -- Remember where we are and draw relative to that:
-  local drawFrom = ui.getCursor()
-
-  -- Just draw the map itself:
-  ui.drawImage(mapFilename, drawFrom, drawFrom + mapSize)
-
-  -- Go through all the cars:
-  for i = 1, sim.carsCount do
-
-    -- Again, 0-based indices and, again, only connected ones
-    local car = ac.getCar(i - 1)
-    if car.isConnected then
-
-      -- Simple transformation from world to relative coordinates:
-      local posX = (car.position.x + mapParams.X_OFFSET) / mapParams.WIDTH
-      local posY = (car.position.z + mapParams.Z_OFFSET) / mapParams.HEIGHT
-
-      -- And just draw a filled circle there. In blue if car is selected:
-      ui.drawCircleFilled(drawFrom + vec2(posX, posY) * mapSize, 4, car == selectedCar and rgbm.colors.blue or rgbm.colors.red)
-
+  end
+  if comboMeter > 10 and math.random() > 0.98 then
+    for i = 1, math.floor(comboMeter) do
+      local dir = vec2(math.random() - 0.5, math.random() - 0.5)
+      glitterCount = glitterCount + 1
+      glitter[glitterCount] = { 
+        color = rgbm.new(hsv(math.random() * 360, 1, 1):rgb(), 1), 
+        pos = vec2(195, 75) + dir * vec2(40, 20),
+        velocity = dir:normalize():scale(0.2 + math.random()),
+        life = 0.5 + 0.5 * math.random()
+      }
     end
-
-  end
-
-  -- All `ui.draw…` functions don’t actually move cursor, so we’re still where we were when started drawing stuff. Let’s move with
-  -- map size, so that window size would extend and include map:
-  ui.dummy(mapSize)
-
-  -- Using `ui.dummy()` like that also allows to easily track if map was clicked:
-  if ui.itemClicked() then
-    -- If it was, let’s update destination point:
-    teleportPoint = (ui.mouseLocalPos() - drawFrom) / mapSize
-  end
-
-  -- And draw destination point in green:
-  ui.drawCircleFilled(drawFrom + teleportPoint * mapSize, 4, rgbm.colors.green)
-end
-
--- This last function is the one that will be called when “OK” is pressed. Note: if you were to change call and add flag
--- `ui.OnlineExtraFlags.Tool`, there wouldn’t be an “OK” button, you’d have to add one yourself with `ui.button()` or something.
-local function teleportHUDClosed(okClicked)
-  -- Function will be called if tool is closed with cancellation too, in case we’d want to dispose of something. But we’re
-  -- only interested in it closing with “OK”, and if something was selected:
-  if okClicked and selectedCar then
-
-    -- Let’s get world coordinates using that simple function. Or, you can just use something like
-    -- `ac.trackProgressToWorldCoordinate(trackProgress)` to teleport car on track, not where the click was (just move
-    -- the line defining `teleportPoint` above).
-    local worldCoordinates = getWorldPosFromRelativePos(teleportPoint)
-
-    -- And calculate direction in a sort of similar way. Just find nearest point on track and nearest point on track one meter in front, and
-    -- use the difference as direction:
-    local trackProgress = ac.worldCoordinateToTrackProgress(worldCoordinates)
-    local worldDirection = (ac.trackProgressToWorldCoordinate(trackProgress + 1 / sim.trackLengthM) - ac.trackProgressToWorldCoordinate(trackProgress)):normalize()
-
-    -- And emit the event:
-    jumpEvent({
-      targetSessionID = selectedCar.sessionID,
-      targetPosition = worldCoordinates,
-      targetDirection = worldDirection
-    })
   end
 end
 
--- And, finally, register the tool:
-ui.registerOnlineExtra(ui.Icons.FastForward, 'Teleport somebody', nil, teleportHUD, teleportHUDClosed, ui.OnlineExtraFlags.Admin)
+local speedWarning = 0
+function script.drawUI()
+  if not config.showUI then return end
 
+  local uiState = ac.getUI()
+  updateMessages(uiState.dt)
 
--- Also, one last thing. 0.1.77 and a lot of 0.1.78-preview builds have this problem where sometimes tool wouldn’t appear
--- when opened. That’ll be fixed with 0.1.78.
+  local speedRelative = math.saturate(math.floor(ac.getCarState(1).speedKmh) / config.minimumSpeed)
+  speedWarning = math.applyLag(speedWarning, speedRelative < 1 and 1 or 0, 0.5, uiState.dt)
+
+  local colorDark = rgbm(0.4, 0.4, 0.4, 1)
+  local colorGrey = rgbm(0.7, 0.7, 0.7, 1)
+  local colorAccent = rgbm.new(hsv(speedRelative * 120, 1, 1):rgb(), 1)
+  local colorCombo = rgbm.new(hsv(comboColor, math.saturate(comboMeter / 10), 1):rgb(), math.saturate(comboMeter / 4))
+
+  local function speedMeter(ref)
+    ui.drawRectFilled(ref + vec2(0, -4), ref + vec2(180, 5), colorDark, 1)
+    ui.drawLine(ref + vec2(0, -4), ref + vec2(0, 4), colorGrey, 1)
+    ui.drawLine(ref + vec2(config.minimumSpeed, -4), ref + vec2(config.minimumSpeed, 4), colorGrey, 1)
+
+    local speed = math.min(ac.getCarState(1).speedKmh, 180)
+    if speed > 1 then
+      ui.drawLine(ref + vec2(0, 0), ref + vec2(speed, 0), colorAccent, 4)
+    end
+  end
+
+  ui.beginTransparentWindow("overtakeScore", vec2(uiState.windowSize.x * 0.5 - 600, 100), vec2(400, 400), false)
+  ui.beginOutline()
+
+  ui.pushStyleVar(ui.StyleVar.Alpha, 1 - speedWarning)
+  ui.pushFont(ui.Font.Title)
+  ui.text("Overtake Run")
+  ui.popFont()
+  ui.popStyleVar()
+
+  local pbText = "PB: " .. personalBest .. " pts"
+  if ownRank > 0 then
+    pbText = pbText .. " (" .. ownRank .. ".)"
+  end
+  ui.text(pbText)
+
+  ui.pushFont(ui.Font.Huge)
+  ui.text(totalScore .. " pts")
+  ui.sameLine(0, 40)
+  if comboMeter > 20 then
+    ui.beginRotation()
+  end
+  ui.textColored(math.ceil(comboMeter * 10) / 10 .. "x", colorCombo)
+  if comboMeter > 20 then
+    ui.endRotation(math.sin(comboMeter / 180 * 3141.5) * 3 * math.lerpInvSat(comboMeter, 20, 30) + 90)
+  end
+  ui.popFont()
+
+  if currentRank > 0 then
+    ui.offsetCursorY(-5)
+    ui.pushFont(ui.Font.Title)
+    ui.text("Current Rank: " .. currentRank .. ".")
+    ui.popFont()
+  end
+  
+  ui.endOutline(rgbm(0, 0, 0, 0.3))
+
+  ui.offsetCursorY(20)
+  ui.pushFont(ui.Font.Title)
+  local startPos = ui.getCursor()
+  for i = 1, #messages do
+    local m = messages[i]
+    local f = math.saturate(4 - m.currentPos) * math.saturate(8 - m.age)
+    ui.setCursor(startPos + vec2(20 + math.saturate(1 - m.age * 10) ^ 2 * 100, (m.currentPos - 1) * 30))
+    ui.beginOutline()
+    ui.textColored(m.text, m.mood == 1 and rgbm(0, 1, 0, f) or m.mood == -1 and rgbm(1, 0, 0, f) or rgbm(1, 1, 1, f))
+    ui.endOutline(rgbm(0, 0, 0, 0.3 * f))
+  end
+  for i = 1, glitterCount do
+    local g = glitter[i]
+    if g ~= nil then
+      ui.drawLine(g.pos, g.pos + g.velocity * 4, g.color, 2)
+    end
+  end
+  ui.popFont()
+  ui.setCursor(startPos + vec2(0, 4 * 30))
+
+  ui.pushStyleVar(ui.StyleVar.Alpha, speedWarning)
+  ui.setCursorY(0)
+  ui.pushFont(ui.Font.Main)
+  ui.textColored("Keep speed above " .. config.minimumSpeed .. " km/h:", colorAccent)
+  speedMeter(ui.getCursor() + vec2(-9, 4))
+  ui.popFont()
+  ui.popStyleVar()
+
+  ui.endTransparentWindow()
+end
+
+--[[
+  This script is based on the overtake mode by x4fab licensed under the MIT license:
+  https://github.com/ac-custom-shaders-patch/acc-lua-internal/blob/main/included-new-modes/overtake/mode.lua
+
+  Please note that the MIT license only applies to code written by x4fab.
+  All other parts of this file are (c) 2023 AssettoServer Development Team.
+
+  MIT License
+
+  Copyright (c) 2022 Ilja Jusupov
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+]]
