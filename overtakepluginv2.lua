ebbcd83e7783c17a01728cc7d86a083b50e9d1b0
iLLUS1ONpt <34459615+iLLUS1ONpt@users.noreply.github.com>
2026-03-26 16:37:36 -0100

    New collision check test v2 (ignores wall taps)
    Previous one crashed so testing this one which is based on speed difference.

--- overtakepluginv2.lua
+++ overtakepluginv2.lua
@@ -133,19 +133,22 @@ function script.update(dt)
             --if car.collidedWith == 0 then
                 --state.collided = true
 
-            --NEW COLLISION CHECK
-            if player.collisionDepth > 0.08 then
+            --NEW COLLISION CHECK START
+            local impactSpeed = math.abs(player.speedKmh - car.speedKmh)
+
+            if car.collidedWith == 0 and impactSpeed > 25 then
                 state.collided = true
-
-            if totalScore > highestScore then
-                highestScore = math.floor(totalScore)
-                stored.playerscore:set(highestScore)
-                ac.sendChatMessage("has a new highscore of " .. totalScore .. " pts!")
-            end
-
+            
+                if totalScore > highestScore then
+                    highestScore = math.floor(totalScore)
+                    stored.playerscore:set(highestScore)
+                    ac.sendChatMessage("has a new highscore of " .. totalScore .. " pts!")
+                end
+            
                 totalScore = 0
                 comboMeter = 1
             end
+            --NEW COLLISION CHECK END
 
         		if totalScore > highestScore then
             		highestScore = math.floor(totalScore)
